85e1de594a1faf7607febf9e43a92d1ab1635db9
MD.Shiblu <mdshiblu.m9086318@gmail.com>
2021-05-02 06:50:22 -0700

    change files

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -17,15 +17,15 @@ const About = () => {
                     <div style={{ borderLeft: '2px solid lightgray', marginLeft: '20px' }}>
                         <h2>About Me</h2>
                         <p>I am a junior web developer with More than three months experience i this field</p>
-                        <a className="resume" href="https://drive.google.com/file/d/1PlgAFnlPzWdqvS3sGROwZ0ZeoOZBZedA/view?usp=sharing" target="_blank">Resume</a>
+                        <a className="resume" href="https://drive.google.com/file/d/1PlgAFnlPzWdqvS3sGROwZ0ZeoOZBZedA/view?usp=sharing">Resume</a>
                     </div>
                 </div>
-                <div style={{margin:'-50px 0 0 350px'}}>
+                <div style={{ margin: '-50px 0 0 350px' }}>
                     <ul className="icon">
-                        <li><a href="https://www.facebook.com/mdshiblu822021/" target="_blank"><FontAwesomeIcon className="icon active-icon" icon={faFacebookF} /></a></li>
-                        <li><a href="https://github.com/mdshiblu9086318" target="_blank"><FontAwesomeIcon className="icon active-icon" icon={faGithub} /></a></li>
-                        <li><a href="https://www.linkedin.com/in/md-shiblu-b6b433211/" target="_blank"><FontAwesomeIcon className="icon active-icon" icon={faLinkedin} /></a></li>
-                        <li><a href="https://twitter.com/?lang=en" target="_blank"><FontAwesomeIcon className="icon active-icon" icon={faTwitter} /></a></li>
+                        <li><a href="https://www.facebook.com/mdshiblu822021/"><FontAwesomeIcon className="icon active-icon" icon={faFacebookF} /></a></li>
+                        <li><a href="https://github.com/mdshiblu9086318"><FontAwesomeIcon className="icon active-icon" icon={faGithub} /></a></li>
+                        <li><a href="https://www.linkedin.com/in/md-shiblu-b6b433211/"><FontAwesomeIcon className="icon active-icon" icon={faLinkedin} /></a></li>
+                        <li><a href="https://twitter.com/?lang=en"><FontAwesomeIcon className="icon active-icon" icon={faTwitter} /></a></li>
                     </ul>
                 </div>
             </div>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,10 +12,10 @@ const Home = () => {
             <div>
                 <div>
                     <ul className="social-icon">
-                        <li><a href="https://www.facebook.com/mdshiblu822021/" target="_blank"><FontAwesomeIcon className="icon active-icon" icon={faFacebookF} /></a></li>
-                        <li><a href="https://github.com/mdshiblu9086318" target="_blank"><FontAwesomeIcon className="icon active-icon" icon={faGithub} /></a></li>
-                        <li><a href="https://www.linkedin.com/in/md-shiblu-b6b433211/" target="_blank"><FontAwesomeIcon className="icon active-icon" icon={faLinkedin} /></a></li>
-                        <li><a href="https://twitter.com/?lang=en" target="_blank"><FontAwesomeIcon className="icon active-icon" icon={faTwitter} /></a></li>
+                        <li><a href="https://www.facebook.com/mdshiblu822021/"><FontAwesomeIcon className="icon active-icon" icon={faFacebookF} /></a></li>
+                        <li><a href="https://github.com/mdshiblu9086318"><FontAwesomeIcon className="icon active-icon" icon={faGithub} /></a></li>
+                        <li><a href="https://www.linkedin.com/in/md-shiblu-b6b433211/"><FontAwesomeIcon className="icon active-icon" icon={faLinkedin} /></a></li>
+                        <li><a href="https://twitter.com/?lang=en"><FontAwesomeIcon className="icon active-icon" icon={faTwitter} /></a></li>
                     </ul>
                 </div>
             </div>
